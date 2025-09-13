9d856788c7210fdd969775ef42960e4f21c3324d
aditya8186 <adityapalakkuzhy5@gmail.com>
2025-09-13 23:20:44 +0530

    add sys.path fix for colab imports

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import os
+import sys
+# Ensure project root is on sys.path so 'src' imports work when running as a script
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 import json
 import time
 import argparse
